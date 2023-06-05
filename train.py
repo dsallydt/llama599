a33c324bc86c79c28bc485017c7cac45ab1eaf44
Sally Dong <sallyqd@cs.washington.edu>
2023-06-05 14:05:04 -0700

    fill in some details for training

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from llama import Transformer
+from llama import ModelArgs, Transformer, Tokenizer, LLaMA
+from example import load
 
 
 class LLMDataset(Dataset):
@@ -17,7 +18,7 @@ class LLMDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx]
 
-def load_data():
+def load_data(batch_size=64):
     with open('00.jsonl', 'r') as f:
         data = json.load(f)
 
@@ -35,26 +36,48 @@ def load_data():
     train_dataset = LLMDataset(train_data)
     val_dataset = LLMDataset(val_data)
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size, shuffle=False)
 
     return train_loader, val_loader, vocab_size
 
+def train_model():
+    # training hyperparameters
+    num_epochs = 50
+    batch_size = 64
+    learning_rate = 1e-3
+    log_interval = 25
+    
+    # TODO: model architecture...put in model file?
+    temperature = 0.8 
+    top_p = 0.95
+    max_gen_length = 256
+    dim = 2048
+    n_heads = 4
+    n_layers = 4
+    max_seq_len = 512 # not sure
+    
+    vocab_size = 32000 #TODO: this should be set by the Tokenizer automatically
+    training_sample = 30000
+    
+    ckpt_dir = "" # checkpoint directory
+    tokenizer_path = "" 
 
-def train_model(data, params, vocab_size, output_size, num_epochs, learning_rate):
-    model = Transformer(params, vocab_size) #TODO
-
+    lm : LLaMA
+    lm = load(ckpt_dir, tokenizer_path, -1, -1, max_seq_len, batch_size)
+    data = load_data(batch_size)
+    
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(lm.model.params, lr=learning_rate)
 
     for epoch in range(num_epochs):
-        for input, target in data:
+        for input, target in data: # input is List[str] of prompts
             optimizer.zero_grad()
-            output = model(input)
-            loss = criterion(output.view(-1, output_size), target.view(-1))
-            loss.backward()
+            output = lm.generate(input, max_gen_length, temperature, top_p) #TODO: should we use generate for FF?
+            loss = criterion(output.view(-1, max_gen_length), target.view(-1)) #TODO: 
+            loss.backward() #TODO:
             optimizer.step()
 
         print(f'Epoch: {epoch}/{num_epochs}, Loss: {loss.item()}')
-        torch.save(model.state_dict(), f'epoch{epoch}-language_model.pth')
-    torch.save(model.state_dict(), 'language_model.pth')
+        torch.save(lm.model.state_dict(), f'epoch{epoch}-language_model.pth')
+    torch.save(lm.model.state_dict(), 'language_model.pth')
